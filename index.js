80cdb195eb638850261826da48c434f1eba42f1a
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:29:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+const countries = ["USA", "Pakistan", "Canada", "Japan", "China"];
+console.log("List of Countries:");
+for (let Country of countries) {
+    console.log(Country);
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+const countries: string[] = ["USA","Pakistan","Canada","Japan","China"]
+
+console.log("List of Countries:")
+for (let Country of countries)
+{
+console.log(Country)
+}
